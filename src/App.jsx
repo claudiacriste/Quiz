787f1fc55abc6255ce4b7ba087e49b43582974ed
claudiacriste <153808443+claudiacriste@users.noreply.github.com>
2024-02-15 07:11:28 +0000

    Quiz

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,52 @@
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const intrebari=[
+    {
+      intrebare:"Care este capitala Romaniei?",
+      variante:["Paris", "Londra", "Bucuresti", "Roma"],
+      varc:"Bucuresti",
+    },
+    {
+      intrebare:"Care este cel mai inalt munte din lume?",
+      variante:["Munti Anzi", "Muntele Etna", "Munti Carpati", "Munti Hymalaia"],
+      varc:"Munti Hymalaia",
+    },
+    {
+      intrebare:"Cate zile are un an bisect?",
+      variante:["366", "365", "364", "363"],
+      varc:"366",
+    },
+    {
+      intrebare:"Cine a scris Hamlet?",
+      variante:["William Shakespeare", "George Bernard Shaw", "Anton Cehov", "Moliere"],
+      varc:"William Shakespeare",
+    },
+    {
+      intrebare:"Care este culoarea cerului in timpul zilei?",
+      variante:["Verde", "Albastru", "Rosu", "Portocaliu"],
+      varc:"Albastru",
+    }
+  ]
+  const[index, setIndex]=useState(0)
+  function esteadevarata(raspuns){
+   if(raspuns===intrebari[index].varc){
+    setIndex(index+1)
+   }
+   else{
+    setIndex(index)
+   }
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      <h3>{intrebari[index].intrebare}</h3>
+      <div className="optiuni">
+      {intrebari[index].variante.map((option, index) => (
+  <button key={index} onClick={() => esteadevarata(option)}>{option}</button> 
+))}
+
+      </div>
     </div>
   );
 }
